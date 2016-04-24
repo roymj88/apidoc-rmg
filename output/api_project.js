4472446102722cf49e742ffbf1c317909a2b623e
Roy M J <roymj88@gmail.com>
2016-04-24 22:17:47 +0530

    Adding output folder

---
+++ output/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "RMG Proto 2",
+  "version": "0.0.1",
+  "description": "API Documentation for Page 3 and Page 4",
+  "title": "API Doc: RMG Proto 2",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2015-09-09T13:09:34.256Z",
+    "url": "http://apidocjs.com",
+    "version": "0.13.1"
+  }
+});
